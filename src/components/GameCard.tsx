05fa1f5166449c98c1e2c78ee79a16798e46a0b4
Kimani Gopaul <kgopaul1@live.com>
2023-11-10 12:48:12 -0500

    Display genres

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -12,7 +12,7 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card>
-      <Image src={getCroppedImageUrl(game.background_image)} />
+      <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent="space-between">
