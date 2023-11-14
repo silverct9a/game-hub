6a81e957c59e0984aca8ecc5ea9b348d7df63a2e
Kimani Gopaul <kgopaul1@live.com>
2023-11-14 11:54:01 -0500

    Build sort selector

---
+++ src/components/SortSelector.1.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const SortSelector = () => {
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+        {selectedPlatform?.name || "Platforms"}
+      </MenuButton>
+      <MenuList>
+        {data.map((platform) => (
+          <MenuItem
+            onClick={() => onSelectPlatform(platform)}
+            key={platform.id}
+          >
+            {platform.name}
+          </MenuItem>
+        ))}
+      </MenuList>
+    </Menu>
+  );
+};
